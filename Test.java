7d6be2cf1988608bd967957aff537869f6d0e9c3
HansLee <hwansu29@naver.com>
2023-07-31 15:27:29 +0900

    modify Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Testing Git Hub - changed 2");
+        System.out.println("Testing Git Hub - changed");
     }
 }
